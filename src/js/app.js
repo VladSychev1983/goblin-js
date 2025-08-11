73ebf26bb723fa0906b6a26004d9c1fd7896f6ce
Vladislav Sychev <vladsychev1983@gmail.com>
2025-08-11 18:07:30 +0300

    Resolved

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import Game from "./game";
 console.log("app.js included");
-const game = document.querySelector('.game');
+const game = document.querySelector(".game");
 const play = new Game(game);
 play.start();
--- src/js/game.js
+++ src/js/game.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line prettier/prettier
 export default class Game {
   constructor(dom) {
     this.GameContainer = dom.querySelector(".game-container");
@@ -6,26 +5,51 @@ export default class Game {
     this.MissedCounter = dom.querySelector(".missed-counter");
     this.CurrentPosition = null;
     this.GameContainerSize = this.GameContainer.children.length;
+    this.killed = 0;
+    this.missed = 0;
   }
   start() {
     this.KilledCounter.textContent = 0;
     this.MissedCounter.textContent = 0;
-    setInterval(() => {
-        let pos = this.PositionGenerator();
-        let gameItem = this.GameContainer.children[pos];
-        gameItem.classList.add('goblin');
-        console.log('current pos:' + pos);
-        const timerId = setTimeout(() => {
-        console.log('Прошлa 1 секунда');
-        gameItem.classList.remove('goblin');
-        }, 1000)
-  }, 2000);
+    let timerInt = setInterval(() => {
+      let pos = this.PositionGenerator();
+      let gameItem = this.GameContainer.children[pos];
+      gameItem.classList.add("goblin");
+      console.log("current pos:" + pos);
+      gameItem.addEventListener("click", () => {
+        //увеличиваем счетчик если при клике есть класс гоблин
+        if (gameItem.classList.contains("goblin")) {
+          this.killed++;
+        }
+        console.log(this.killed);
+        console.log(gameItem.classList.contains("goblin"));
+      });
+      const timerId = setTimeout(() => {
+        console.log("Прошлa 1 секунда");
+        gameItem.classList.remove("goblin");
+        if (this.killed >= 10) {
+          alert("Победа!");
+          clearInterval(timerInt);
+        }
+        if (this.missed >= 10) {
+          alert("Вы проиграли!");
+          clearInterval(timerInt);
+        }
+        console.log("killed-", this.killed);
+        console.log("killedTextContent-", this.KilledCounter.textContent);
+        if (this.killed == this.KilledCounter.textContent) {
+          this.missed++;
+          this.MissedCounter.textContent = this.missed;
+        }
+        this.KilledCounter.textContent = this.killed;
+      }, 700);
+    }, 3000);
   }
 
   PositionGenerator() {
     let pos = Math.floor(Math.random() * this.GameContainerSize);
     while (pos === this.CurrentPosition) {
-        pos = Math.floor(Math.random() * this.GameContainerSize);
+      pos = Math.floor(Math.random() * this.GameContainerSize);
     }
     this.CurrentPosition = pos;
     return pos;
